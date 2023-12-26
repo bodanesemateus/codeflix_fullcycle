ee52d7e139e61ef9d7feb8a87d2932fe7da54ee6
bodanesemateus <bodanesemateus@gmail.com>
2023-12-26 22:24:08 +0000

    create repositorio para trabalhar em memoria, onde trato insert, bulk insert, update, delete, findbyid e findall

---
+++ src/shared/db/in-memory/in-memory.repository.ts
@@ -0,0 +1,58 @@
+import { Entity } from "../../domain/entity";
+import { RepositoryInterface } from "../../domain/respository/repository-interface";
+import { ValueObject } from "../../domain/value-object";
+
+
+
+export abstract class InMemoryRepository<E extends Entity, EntityId extends ValueObject> 
+    implements RepositoryInterface<E, EntityId>
+    {
+        items: E[] = [];
+
+        async insert(entity: E): Promise<void> {
+            this.items.push(entity);
+        }
+        
+        async bulkInsert(entities: any[]): Promise<void> {
+            this.items.push(...entities);
+        }
+        
+        async update(entity: E): Promise<void> {
+            const indexFound = this.items.findIndex((item) =>
+                 item.entity_id.equals(entity.entity_id)
+            );
+
+            if (indexFound === -1) {
+                throw new Error('Entity not found');
+            }
+            this.items[indexFound] = entity;
+        }
+
+        async delete(entity: EntityId): Promise<void> {
+            const indexFound = this.items.findIndex((item) =>
+                 item.entity_id.equals(entity)
+            );
+
+            if (indexFound === -1) {
+                throw new Error('Entity not found');
+            }
+            this.items.splice(indexFound, 1);
+        }
+        
+        async findById(entity: EntityId): Promise<E> {
+            const item = this.items.find(item => item.entity_id.equals(entity));
+            return typeof item === 'undefined' ? null : item;
+        }
+
+        // protected _get(entity_id: EntityId) {
+        //     const item = this.items.find(item => item.entity_id.equals(entity_id));
+        //     return typeof item === 'undefined' ? null : item;
+        // }
+
+        async findAll(): Promise<any[]> {
+            return this.items;
+        }
+        
+        abstract getEntityName(): new (...args: any[]) => E;
+
+}
