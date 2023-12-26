85554f449ad5f9989e65788aa8d53d4275fd9245
bodanesemateus <bodanesemateus@gmail.com>
2023-12-26 22:23:15 +0000

    alterado propriedade de delete, para receber o ID da entidade. alterado o findbyid para poder retornar null;

--- src/shared/domain/respository/repository-interface.ts
+++ src/shared/domain/respository/repository-interface.ts
@@ -8,9 +8,9 @@ export interface RepositoryInterface<E extends Entity, EntityId extends ValueObj
     insert(entity: E): Promise<void>;
     bulkInsert(entities: E[]): Promise<void>;
     update(entity: E): Promise<void>;
-    delete(entity: E): Promise<void>;
+    delete(entity: EntityId): Promise<void>;
 
-    findById(id: EntityId): Promise<E>;
+    findById(id: EntityId): Promise<E | null>;
     findAll(): Promise<E[]>;
 
 
