27322d47e73d52288a65ea2e2f4d0724ef1f7906
bodanesemateus <bodanesemateus@gmail.com>
2023-12-26 20:15:41 +0000

    alterado chamada de arquivo de - para .

--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -2,7 +2,7 @@ import { Entity } from "../../shared/domain/entity";
 import { EntityValidationError } from "../../shared/domain/validators/validation.error";
 import { ValueObject } from "../../shared/domain/value-object";
 import { Uuid } from "../../shared/domain/values-objects/uuid.vo";
-import { CategoryValidatorFactory } from "./category-validator";
+import { CategoryValidatorFactory } from "./category.validator";
 
 
 export type CategoryConstructorProps = {
